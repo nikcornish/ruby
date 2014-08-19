8b2b78ad91028e8a4ea669e27193a7cd11cd7052
Nik Cornish <nik.cornish@gmail.com>
2014-08-19 21:48:05 +0200

    initial commit, upto ex3

---
+++ ex2.rb
@@ -0,0 +1,11 @@
+# This is a comment
+# Anything after the # is ignored by ruby.
+
+puts "I could have code like this." # and this is ignored.
+
+# You can also use a comment to 'disable' code running eg:
+# puts "This won't run."
+
+puts "This will run."
+puts "This is # inside a string."
+
---
+++ ex3.rb
@@ -0,0 +1,40 @@
+# you're upto ... http://ruby.learncodethehardway.org/book/ex3.html
+
+# +	addition
+# - subtraction
+# / division
+# * multiplication
+# % modulo (explain modulo here)
+# < less than
+# > greater than
+# <= less than or equal to
+# >= greater than or equal to
+
+# this puts out a basic string
+puts "I will now count my chickens:"
+
+# this puts out 'Hens', then the result (bodmas style)
+puts "Hens #{25+30/6}"
+puts "Roosters #{100-25*3%4}"
+
+# this puts out a basic string
+puts "Now I will count the eggs:"
+
+puts 3+2+1-5+4%2-1/4+6
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+puts 3 + 2 < 5 - 7
+
+puts "What is 3+2? #{3+2}"
+puts "What is 5-7 #{5-7}"
+
+# this puts out a basic string
+puts "Oh, that's why it's false."
+
+# this puts out a basic string
+puts "How about some more."
+
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
